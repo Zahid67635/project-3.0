8a1c7a4067314eda453ae04436de34dc93b1d66e
zahid-hasan <zahid67635@gmail.com>
2023-11-20 12:59:47 +0600

    created component route

---
+++ src/app/components/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-100">
+      {/* add your components here */}
+    </div>
+  );
+}
+
+export default page;
